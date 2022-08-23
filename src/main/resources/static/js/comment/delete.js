2c56243c3335cd521b0199c6e1c3f752e058dc46
seoli0179 <seoli0179@kakao.com>
2022-08-23 16:21:23 +0900

    comment 삭제 의사 메세지 추가

--- src/main/resources/static/js/comment/delete.js
+++ src/main/resources/static/js/comment/delete.js
@@ -2,27 +2,30 @@ $(document).ready(function () {
 
 });
 
-function deleteComment(commentId){
-    $.ajax({
-        type: "POST",
-        url: "/comment/deleteComment",
-        data: {
-            "commentId": commentId
-        },
-        success: function (data) {
-            if (data == "SUCCESS") {
-                alert("삭제 완료!");
-                $("#comment-box").load(window.location.href + " #comment-box > *"); //띄어쓰기 주의!!!
-            } else {
-                alert("삭제 실패!");
-            }
+function deleteComment(commentId) {
 
-        },
-        error: function () {
-            console.log('fail');
-            alert("오류 발생!");
-        }
-    });
+    if (confirm("댓글을 삭제하시겠습니까?")) {
+        $.ajax({
+            type: "POST",
+            url: "/comment/deleteComment",
+            data: {
+                "commentId": commentId
+            }, success: function (data) {
+                if (data == "SUCCESS") {
+                    alert("삭제 완료!");
+                    $("#comment-box").load(window.location.href + " #comment-box > *"); //띄어쓰기 주의!!!
+                } else {
+                    alert("삭제 실패!");
+                }
+
+            }, error: function () {
+                console.log('fail');
+                alert("오류 발생!");
+            }
+        });
+    }
+
+
 }
 
 
