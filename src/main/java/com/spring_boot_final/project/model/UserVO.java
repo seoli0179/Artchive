c29205c014d01ee8e59f2da1c825e69d83bbe052
xangjong <csj7080@naver.com>
2022-08-29 00:29:08 +0900

    UserVO,java, IUserDAO .java, userMapper.xml, UserSevice.java, MypageController.java, finaldb_user.sql add : select 기능 추가

--- src/main/java/com/spring_boot_final/project/model/UserVO.java
+++ src/main/java/com/spring_boot_final/project/model/UserVO.java
@@ -20,88 +20,121 @@ public class UserVO {
     private Date createdAt;
     @DateTimeFormat(pattern="yyyy-MM-dd")
     private Date updatedAt;
-
+  
+    // 유저 이름, 전화번호 추가
+    private String userName;
+    private String userNum;
+    
     public UserVO() {
     }
+    
+    public UserVO(String userId, String userPw, String userEmail, String userNickname, String userGender,
+			Date userBirth, String userName, String userNum) {
+		super();
+		this.userId = userId;
+		this.userPw = userPw;
+		this.userEmail = userEmail;
+		this.userNickname = userNickname;
+		this.userGender = userGender;
+		this.userBirth = userBirth;
+		this.userName = userName;
+		this.userNum = userNum;
+	}
+    
+    // 모름 UserDateController 에러나서 추가함
+	public UserVO(String id, String encode, String email, String nickname, String gender, Date parse) {
+		// TODO Auto-generated constructor stub
+	}
 
-    public UserVO(String userId, String userPw, String userEmail, String userNickname, String userGender, Date userBirth) {
-        this.userId = userId;
-        this.userPw = userPw;
-        this.userEmail = userEmail;
-        this.userNickname = userNickname;
-        this.userGender = userGender;
-        this.userBirth = userBirth;
-    }
+	public String getUserId() {
+		return userId;
+	}
 
-    public String getUserId() {
-        return userId;
-    }
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
 
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
+	public String getUserPw() {
+		return userPw;
+	}
 
-    public String getUserPw() {
-        return userPw;
-    }
+	public void setUserPw(String userPw) {
+		this.userPw = userPw;
+	}
 
-    public void setUserPw(String userPw) {
-        this.userPw = userPw;
-    }
+	public String getUserEmail() {
+		return userEmail;
+	}
 
-    public String getUserEmail() {
-        return userEmail;
-    }
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
 
-    public void setUserEmail(String userEmail) {
-        this.userEmail = userEmail;
-    }
+	public String getUserNickname() {
+		return userNickname;
+	}
 
-    public String getUserNickname() {
-        return userNickname;
-    }
+	public void setUserNickname(String userNickname) {
+		this.userNickname = userNickname;
+	}
 
-    public void setUserNickname(String userNickname) {
-        this.userNickname = userNickname;
-    }
+	public String getUserGender() {
+		return userGender;
+	}
 
-    public String getUserGender() {
-        return userGender;
-    }
+	public void setUserGender(String userGender) {
+		this.userGender = userGender;
+	}
 
-    public void setUserGender(String userGender) {
-        this.userGender = userGender;
-    }
+	public Date getUserBirth() {
+		return userBirth;
+	}
 
-    public Date getUserBirth() {
-        return userBirth;
-    }
+	public void setUserBirth(Date userBirth) {
+		this.userBirth = userBirth;
+	}
 
-    public void setUserBirth(Date userBirth) {
-        this.userBirth = userBirth;
-    }
+	public RollState getUserRoll() {
+		return userRoll;
+	}
 
-    public RollState getUserRoll() {
-        return userRoll;
-    }
+	public void setUserRoll(RollState userRoll) {
+		this.userRoll = userRoll;
+	}
 
-    public void setUserRoll(RollState userRoll) {
-        this.userRoll = userRoll;
-    }
+	public Date getCreatedAt() {
+		return createdAt;
+	}
 
-    public Date getCreatedAt() {
-        return createdAt;
-    }
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
 
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
 
-    public Date getUpdatedAt() {
-        return updatedAt;
-    }
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
 
-    public void setUpdatedAt(Date updatedAt) {
-        this.updatedAt = updatedAt;
-    }
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserNum() {
+		return userNum;
+	}
+
+	public void setUserNum(String userNum) {
+		this.userNum = userNum;
+	}
+    
+	
+
+    
 }
