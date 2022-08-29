5a1500b64aa3b418eb0a2e154a87da30b9ff0d7b
saehim <saeilee.vxv@gmail.com>
2022-08-29 23:41:56 +0900

    courseViewController_java Update: select detailView 구현

--- src/main/java/com/spring_boot_final/project/controller/course/CourseViewController.java
+++ src/main/java/com/spring_boot_final/project/controller/course/CourseViewController.java
@@ -1,17 +1,23 @@
 package com.spring_boot_final.project.controller.course;
 
+import com.spring_boot_final.project.model.CourseRouteVO;
 import com.spring_boot_final.project.model.CourseVO;
 import com.spring_boot_final.project.model.ExhbnVO;
 import com.spring_boot_final.project.service.CourseService;
 import com.spring_boot_final.project.service.ExhbnService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.ArrayList;
+import javax.servlet.http.HttpSession;
+import java.lang.reflect.Array;
+import java.util.*;
 
 @Controller
 public class CourseViewController {
@@ -37,27 +43,64 @@ public class CourseViewController {
     @RequestMapping("/course/list")
     public String selectCourse(Model model) {
         ArrayList<CourseVO> vo = courseService.selectCourse();
+        ArrayList<String[]> tag = new ArrayList<String[]>();
+        for (CourseVO i : vo) {
+            String[] temp = i.getCourseTag().split(";;");
+            tag.add(temp);
+        }
         model.addAttribute("courseList",vo);
+        model.addAttribute("tag", tag);
 
         return "course/courseBoard";
     }
 
     // course !!!test!!!
     @RequestMapping("/course/{courseId}")
-    public String selectCoursePost(@RequestParam int courseId, Model model) {
-
-        CourseVO vo = courseService.selectCoursePost();
+    public String selectCoursePost(@PathVariable("courseId") int courseId,
+                                   HttpSession session,
+                                   Model model) {
+        CourseVO vo = courseService.selectCoursePost(courseId);
         model.addAttribute("course",vo);
+
+        ArrayList<String[]> sites = new ArrayList<String[]>();
+        String[] siteName = vo.getCourseSitesArr().split(";;");
+        String[] siteAddress = vo.getCourseAddressArr().split(";;");
+        String[] siteMemo = vo.getCourseMemoArr().split(";;");
+        model.addAttribute("siteName",siteName);
+        model.addAttribute("siteAddress",siteAddress);
+        model.addAttribute("siteMemo",siteMemo);
 
         return "course/courseDetailView";
     }
 
-    @RequestMapping("/course/detail/edit")
-    public String courseDetailEdit(Model model) {
+    @ResponseBody
+    @RequestMapping("/course/getCourse")
+    public ArrayList<CourseRouteVO> getCourse(int courseId,
+                                              HttpSession session,
+                                              Model model) {
+        CourseVO vo = courseService.selectCoursePost(courseId);
+        String[] siteNames = vo.getCourseSitesArr().split(";;");
+        String[] siteAddresses = vo.getCourseAddressArr().split(";;");
+        String[] siteMemos = vo.getCourseMemoArr().split(";;");
+        ArrayList<CourseRouteVO> result = new ArrayList<>();
 
-        ArrayList<ExhbnVO> vo = service.selectAllData();
-        model.addAttribute("exhbnList",vo);
+        for (int i=0; i<siteNames.length; i++) {
+            CourseRouteVO temp = new CourseRouteVO();
+            temp.setSiteName(siteNames[i]);
+            temp.setSiteAddresses(siteAddresses[i]);
+            temp.setSiteMemos(siteMemos[i]);
+            result.add(temp);
+        }
+        return result;
+    }
 
+
+    @RequestMapping("/course/{courseId}/edit")
+    public String courseDetailEdit(@PathVariable("courseId") int courseId,
+                                   HttpSession session,
+                                   Model model) {
+        CourseVO vo = courseService.selectCoursePost(courseId);
+        model.addAttribute("course",vo);
         return "course/courseDetailEdit";
     }
 
