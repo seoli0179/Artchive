04939cbc7cf330bd8f519a511f98488e24bad852
saehim <saeilee.vxv@gmail.com>
2022-08-31 09:25:13 +0900

    courseDelete_All Update: course 삭제 기능 구현

--- src/main/java/com/spring_boot_final/project/dao/ICourseDAO.java
+++ src/main/java/com/spring_boot_final/project/dao/ICourseDAO.java
@@ -4,6 +4,7 @@ import com.spring_boot_final.project.model.CourseVO;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 @Component
 public interface ICourseDAO {
@@ -11,4 +12,6 @@ public interface ICourseDAO {
     public CourseVO selectCoursePost(int courseId);
 
     public void updateCourse(CourseVO vo);
+
+    public void deleteCourse(HashMap<String, Object> map);
 }
