5112c9916fcdb9231c1a32c8bca3eb5fdce78731
yuna1206yuna5 <yuna1206yuna5@gmail.com>
2022-09-02 21:32:41 +0900

    reviewNote_sql Update: 리뷰게시판 db 수정

--- mysql/finaldb_reviewNote.sql
+++ mysql/finaldb_reviewNote.sql
@@ -1,10 +1,8 @@
-CREATE DATABASE  IF NOT EXISTS `finaldb` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `finaldb`;
--- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
 --
 -- Host: localhost    Database: finaldb
 -- ------------------------------------------------------
--- Server version	8.0.30
+-- Server version	8.0.29
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -18,40 +16,41 @@ USE `finaldb`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `bookmark`
+-- Table structure for table `reviewnote`
 --
 
-DROP TABLE IF EXISTS `reviewNote`;
+DROP TABLE IF EXISTS `reviewnote`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE reviewNote (
-	reviewNoteId int NOT NULL PRIMARY KEY ,
-    reviewNoteTitle	varchar(150) DEFAULT NULL,
-	reviewNote	longtext,
-    reviewNoteLike int DEFAULT '0',
-    reviewNoteCommentNum int DEFAULT '0',
-    reviewNoteCreatedDate datetime default now(),
-    reviewPageViewState varchar(50) DEFAULT 'POST',
-    courseId int,
-    userId  varchar(20) NOT NULL,
-    exhbnId int,
-    KEY `courseId` (`courseId`),
-    KEY `userId` (`userId`),
-	KEY `exhbnId` (`exhbnId`),
-    CONSTRAINT FK_reviewNote_course FOREIGN KEY (courseId) REFERENCES course (courseId),
-	CONSTRAINT FK_reviewNote_user FOREIGN KEY (userId) REFERENCES user (userId),
-    CONSTRAINT FK_reviewNote_exhbn FOREIGN KEY (exhbnId) REFERENCES exhbn (exhbnId)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+CREATE TABLE `reviewnote` (
+  `reviewNoteId` int NOT NULL AUTO_INCREMENT,
+  `reviewNoteTitle` varchar(150) DEFAULT NULL,
+  `reviewNote` longtext,
+  `reviewNoteLike` int DEFAULT '0',
+  `reviewNoteCommentNum` int DEFAULT '0',
+  `reviewNoteCreatedDate` datetime DEFAULT CURRENT_TIMESTAMP,
+  `reviewPageViewState` varchar(50) DEFAULT 'POST',
+  `courseId` int DEFAULT NULL,
+  `userId` varchar(20) NOT NULL,
+  `exhbnId` int DEFAULT NULL,
+  PRIMARY KEY (`reviewNoteId`),
+  KEY `courseId` (`courseId`),
+  KEY `userId` (`userId`),
+  KEY `exhbnId` (`exhbnId`),
+  CONSTRAINT `FK_reviewNote_course` FOREIGN KEY (`courseId`) REFERENCES `course` (`courseId`),
+  CONSTRAINT `FK_reviewNote_exhbn` FOREIGN KEY (`exhbnId`) REFERENCES `exhbn` (`exhbnId`),
+  CONSTRAINT `FK_reviewNote_user` FOREIGN KEY (`userId`) REFERENCES `user` (`userId`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `bookmark`
+-- Dumping data for table `reviewnote`
 --
 
-LOCK TABLES `reviewNote` WRITE;
-/*!40000 ALTER TABLE `reviewNote` DISABLE KEYS */;
-INSERT INTO `reviewNote` VALUES ('1','리뷰게시판 타이틀','리뷰게시판 내용부분 입니다','0','0',default,'POST','1','user2','217842');
-/*!40000 ALTER TABLE `reviewNote` ENABLE KEYS */;
+LOCK TABLES `reviewnote` WRITE;
+/*!40000 ALTER TABLE `reviewnote` DISABLE KEYS */;
+INSERT INTO `reviewnote` VALUES (1,'리뷰게시판 타이틀','리뷰게시판 내용부분 입니다',0,0,'2022-09-02 04:49:38','POST',1,'user',217842);
+/*!40000 ALTER TABLE `reviewnote` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -63,6 +62,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
-alter table reviewNote modify reviewNoteId int not null auto_increment;
-
--- Dump completed on 2022-08-26  9:46:37
+-- Dump completed on 2022-09-02 21:30:37
