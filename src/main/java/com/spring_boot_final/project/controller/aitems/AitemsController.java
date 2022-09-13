2e6a67eb45eda4eb67e7596b0c4f2a3f95a1ae6b
seoli0179 <seoli0179@kakao.com>
2022-09-13 09:53:12 +0900

    aitems 기능 구현

--- src/main/java/com/spring_boot_final/project/controller/aitems/AitemsController.java
+++ src/main/java/com/spring_boot_final/project/controller/aitems/AitemsController.java
@@ -4,27 +4,44 @@ import com.spring_boot_final.project.service.AitemsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 
 
-@Controller
+@RestController
 public class AitemsController {
 
     @Autowired
     AitemsService service;
 
-    @RequestMapping("ai/test")
-    public void aiTest() {
+    @RequestMapping("aitems/relatedItem")
+    public ArrayList<String> relatedItem(@RequestParam("itemId") String itemId) {
 
-        ArrayList<String> items = service.getRelatedItem("209436");
+        //ArrayList<String> items = service.getRelatedItem("209436");
+        ArrayList<String> items = service.getRelatedItem(itemId);
 
-        for(String str : items){
+        for (String str : items) {
             System.out.println(str);
         }
 
+        return items;
 
     }
 
+    @RequestMapping("aitems/personalRecommend")
+    public ArrayList<String> personalRecommend(@RequestParam("userId") String userId) {
+
+        //ArrayList<String> items = service.getRelatedItem("209436");
+        ArrayList<String> items = service.getPersonalRecommend(userId);
+
+        for (String str : items) {
+            System.out.println(str);
+        }
+
+        return items;
+
+    }
 
 }
