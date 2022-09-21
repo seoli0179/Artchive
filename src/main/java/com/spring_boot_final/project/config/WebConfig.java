f7ebb2a2c040491cc77d758b466fd0f4d75906a3
seoli0179 <seoli0179@kakao.com>
2022-09-21 13:53:46 +0900

    image 경로 위치

--- src/main/java/com/spring_boot_final/project/config/WebConfig.java
+++ src/main/java/com/spring_boot_final/project/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.spring_boot_final.project;
+package com.spring_boot_final.project.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
